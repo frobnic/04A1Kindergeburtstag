b5f959410a617bcfb4b3c45e549b37ef830d228e
frank paulsen <frobnic+ita@gmail.com>
2019-08-01 10:40:09 +0200

    Projektdateien hinzufügen.

---
+++ 04A1Kindergeburtstag/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04A1Kindergeburtstag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eier, kinder;
+            int ek, rest;
+
+            Console.Write("Bitte erfassen Sie die Anzahl der Kinder:");
+            kinder = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Bitte erfassen Sie die Anzahl der Eier  :");
+            eier = Convert.ToInt16(Console.ReadLine());
+
+            ek = eier / kinder;
+            rest = eier % kinder;
+
+            Console.WriteLine("Anzahl der Eier je Kind " + ek);
+            Console.WriteLine("Anzahl der Resteier     " + rest);
+
+
+        }
+    }
+}
